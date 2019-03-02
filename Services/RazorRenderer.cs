6cac4877416f8f63643806e72c79560256699838
Jeff Johnson <jjxtra@gmail.com>
2019-03-01 17:26:17 -0700

    Get tests passing again

--- Services/RazorRenderer.cs
+++ Services/RazorRenderer.cs
@@ -5,37 +5,46 @@ using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
 
 namespace MailDemon
 {
-    public class RazorRenderer : IViewRenderService
+    /// <summary>
+    /// Renders razor pages with the absolute minimum setup of MVC, easy to use in console application
+    /// </summary>
+    public class RazorRenderer : IViewRenderService, ILoggerFactory, ILogger
     {
+        private readonly string rootPath;
         private readonly ServiceCollection services;
         private readonly ServiceProvider serviceProvider;
         private readonly ViewRenderService viewRenderer;
 
-        public RazorRenderer()
+        public RazorRenderer(string rootPath)
         {
-            // Initialize the necessary services
+            this.rootPath = rootPath;
             services = new ServiceCollection();
             ConfigureDefaultServices(services);
             serviceProvider = services.BuildServiceProvider();
             viewRenderer = new ViewRenderService(serviceProvider.GetRequiredService<IRazorViewEngine>(), null, null, serviceProvider);
         }
 
-        private static void ConfigureDefaultServices(IServiceCollection services)
+        private void ConfigureDefaultServices(IServiceCollection services)
         {
-            string rootPath = Directory.GetCurrentDirectory();
             var environment = new HostingEnvironment
             {
                 WebRootFileProvider = new PhysicalFileProvider(rootPath),
-                ApplicationName = "RazorRenderer"
+                ApplicationName = typeof(RazorRenderer).Assembly.GetName().Name,
+                ContentRootPath = rootPath,
+                WebRootPath = rootPath,
+                EnvironmentName = "DEVELOPMENT",
+                ContentRootFileProvider = new PhysicalFileProvider(rootPath)
             };
             services.AddSingleton<IHostingEnvironment>(environment);
             services.Configure<RazorViewEngineOptions>(options =>
@@ -44,14 +53,43 @@ namespace MailDemon
                 options.FileProviders.Add(new MailDemonDatabaseFileProvider(rootPath));
             });
             services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
-            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<ILoggerFactory>(this);
+            var diagnosticSource = new DiagnosticListener(environment.ApplicationName);
             services.AddSingleton<DiagnosticSource>(diagnosticSource);
             services.AddMvc();
+        }
+
+        public void Dispose()
+        {
         }
 
         public Task<string> RenderToStringAsync<TModel>(string viewName, TModel model, ExpandoObject viewBag = null, bool isMainPage = false)
         {
             return viewRenderer.RenderToStringAsync(viewName, model, viewBag, isMainPage);
+        }
+
+        void ILoggerFactory.AddProvider(ILoggerProvider provider)
+        {
+            
+        }
+
+        IDisposable ILogger.BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        ILogger ILoggerFactory.CreateLogger(string categoryName)
+        {
+            return this;
+        }
+
+        bool ILogger.IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
+        {
+            return false;
+        }
+
+        void ILogger.Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
         }
     }
 }
